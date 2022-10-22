bc654d23512070241c2f0d974ce9759149f2fafe
Ailing Zhang <ailing@taichi.graphics>
2022-10-23 00:20:30 +0800

    Add pybind11 count_strings demo

--- demo_pybind11.py
+++ demo_pybind11.py
@@ -1,3 +1,14 @@
-from pybind11_demo import add
+from pybind11_demo import add, Response, count_strings
 
 print(add(1, 2))
+
+a = Response()
+print(a.status) # prints "200"
+# a.status = 100
+# The line above will error out as expected
+# Traceback (most recent call last):
+#   File "demo_pybind11.py", line 7, in <module>
+#     a.status = 100
+# AttributeError: can't set attribute
+
+print('count_strings: ', count_strings(["hello", "world"]))
--- src/pybind11_demo.cpp
+++ src/pybind11_demo.cpp
@@ -1,12 +1,41 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include <string>
 
 namespace py = pybind11;
 
 int add(int a, int b) {
   return a + b;
 }
+
+struct Response {
+  int status;
+  std::string reason;
+  std::string text;
+
+  Response (int status, std::string reason, std::string text = " "): 
+    status(status), reason(std::move(reason)), text(std::move(text)) {}
+  
+  Response(): Response(200, "OK") {}
+};
+
 PYBIND11_MODULE(pybind11_demo, m) {
+  py::class_<Response>(m, "Response")
+      .def(py::init<>())
+      .def(py::init<int, std::string>())
+      .def(py::init<int, std::string, std::string>())
+      .def_readonly("status", &Response::status)
+      .def_readonly("reason", &Response::reason)
+      .def_readonly("text", &Response::text);
+  m.def("count_strings", [](py::list list) {
+    int n = 0;
+    for (auto item : list) {
+      if (py::isinstance<py::str>(item)) {
+        n++;
+      }
+    }
+    return n;
+  });
   m.def("add", &add, "Binary add");
 }
